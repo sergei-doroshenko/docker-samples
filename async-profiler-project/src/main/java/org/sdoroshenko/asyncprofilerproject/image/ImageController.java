425b8fb4b5ed43e2f583441ab393a052e6360f32
Sergei Doroshenko <sdoroshenko@edmunds.com>
2019-12-20 19:42:34 +0300

    async-profiler tests

--- async-profiler-project/src/main/java/org/sdoroshenko/asyncprofilerproject/image/ImageController.java
+++ async-profiler-project/src/main/java/org/sdoroshenko/asyncprofilerproject/image/ImageController.java
@@ -1,28 +1,40 @@
 package org.sdoroshenko.asyncprofilerproject.image;
 
-import com.google.common.io.ByteStreams;
-import org.springframework.util.ResourceUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @RestController
 public class ImageController {
 
+    final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
     @GetMapping(value = "/images/{id}")
     public byte[] getImage(@PathVariable("id") String id) throws IOException {
         String fileName = "img" + File.separator + id + ".png";
-        File file = ResourceUtils.getFile("classpath*:" + fileName);
-        InputStream in = new FileInputStream(file);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ByteStreams.copy(in, out);
-        return out.toByteArray();
+        Resource resource = resourceLoader.getResource("classpath:" + fileName);
+        InputStream inputStream = resource.getInputStream();
+
+        try {
+            return FileCopyUtils.copyToByteArray(inputStream);
+        } catch (IOException e) {
+            LOGGER.error("IOException", e);
+        }
+
+        return new byte[0];
     }
 
 }
